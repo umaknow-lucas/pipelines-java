5ee8a78ff5cae633c20e8494539ef40b1ee280de
umaknow-lucas <44575157+umaknow-lucas@users.noreply.github.com>
2020-08-10 16:06:05 -0400

    Update my-script.ps1

--- my-script.ps1
+++ my-script.ps1
@@ -1,3 +1,8 @@
-Write-Output "here is your pipeline workspace" $env:PIPELINE_WORKSPACE
-Write-Output "and again" $env:PIPELINE_WORKSPACE
-Write-Host "and again" $env:PIPELINE_WORKSPACE
+
+If($env:PIPELINE_WORKSPACE) {
+  Write-Output "here is your pipeline workspace" $env:PIPELINE_WORKSPACE
+  Write-Output "and again" $env:PIPELINE_WORKSPACE
+  Write-Host "and again" $env:PIPELINE_WORKSPACE
+} Else {
+  write-host "didnt work"
+}
